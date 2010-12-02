709beba842982c52b12b4ca29e5dc235c66b9256
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2010-12-02 17:30:53 +0000

    HREPTWO-4795: copy trunk to branch for 2.18.xx series

---
+++ trunk/editor/test/src/test/java/org/hippoecm/frontend/editor/impl/DefaultEditorFactoryTest.java
@@ -0,0 +1,336 @@
+/*
+ *  Copyright 2010 Hippo.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hippoecm.frontend.editor.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.version.Version;
+
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.model.IDetachable;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.util.collections.MiniMap;
+import org.hippoecm.frontend.PluginTest;
+import org.hippoecm.frontend.editor.IEditorContext;
+import org.hippoecm.frontend.model.IModelReference;
+import org.hippoecm.frontend.model.JcrNodeModel;
+import org.hippoecm.frontend.model.event.IRefreshable;
+import org.hippoecm.frontend.plugin.IPluginContext;
+import org.hippoecm.frontend.plugin.Plugin;
+import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.plugin.config.impl.JavaPluginConfig;
+import org.hippoecm.frontend.service.EditorException;
+import org.hippoecm.frontend.service.IEditor;
+import org.hippoecm.frontend.service.IEditorFilter;
+import org.hippoecm.frontend.service.IEditorManager;
+import org.hippoecm.frontend.service.IRenderService;
+import org.hippoecm.frontend.service.ITitleDecorator;
+import org.hippoecm.frontend.service.IconSize;
+import org.hippoecm.frontend.service.IEditor.Mode;
+import org.hippoecm.frontend.service.render.RenderPlugin;
+import org.hippoecm.repository.api.HippoNodeType;
+import org.junit.Test;
+
+public class DefaultEditorFactoryTest extends PluginTest {
+    @SuppressWarnings("unused")
+    private final static String SVN_ID = "$Id$";
+
+    static final String EDITORS = "editors";
+    static final String PREVIEWS = "previews";
+    static final String COMPARERS = "compares";
+    static final String FILTERS = "filters";
+    static final String RENDERERS = "service.renderer";
+    
+    public static class Editor extends RenderPlugin {
+        private static final long serialVersionUID = 1L;
+
+        public Editor(IPluginContext context, IPluginConfig config) {
+            super(context, config);
+
+            context.registerService(this, EDITORS);
+        }
+    }
+
+    public static class Preview extends RenderPlugin implements ITitleDecorator {
+        private static final long serialVersionUID = 1L;
+
+        public Preview(IPluginContext context, IPluginConfig config) {
+            super(context, config);
+
+            context.registerService(this, PREVIEWS);
+        }
+
+        public IModel getTitle() {
+            try {
+                return new Model(((JcrNodeModel) getModel()).getNode().getName());
+            } catch (RepositoryException ex) {
+                throw new RuntimeException("failed to determine node name", ex);
+            }
+        }
+
+        public ResourceReference getIcon(IconSize type) {
+            return null;
+        }
+
+    }
+
+    public static class Comparer extends RenderPlugin {
+        private static final long serialVersionUID = 1L;
+
+        public Comparer(IPluginContext context, IPluginConfig config) {
+            super(context, config);
+
+            context.registerService(this, COMPARERS);
+        }
+
+        public IModel getCompareToModel() {
+            return getPluginContext().getService(getPluginConfig().getString("model.compareTo"), IModelReference.class)
+                    .getModel();
+        }
+    }
+
+    public static class CloseFilter extends Plugin implements IEditorFilter {
+        private static final long serialVersionUID = 1L;
+
+        boolean closed = false;
+
+        public CloseFilter(IPluginContext context, IPluginConfig config) {
+            super(context, config);
+
+            IEditor editor = context.getService(config.getString("editor.id"), IEditor.class);
+            context.registerService(this, context.getReference(editor).getServiceId());
+
+            context.registerService(this, FILTERS);
+        }
+
+        public Object preClose() {
+            closed = true;
+            return new Object();
+        }
+
+        public void postClose(Object object) {
+        }
+    }
+
+    private List<IRenderService> getPreviews() {
+        return context.getServices(PREVIEWS, IRenderService.class);
+    }
+
+    private List<IRenderService> getEditors() {
+        return context.getServices(EDITORS, IRenderService.class);
+    }
+
+    private List<IRenderService> getComparers() {
+        return context.getServices(COMPARERS, IRenderService.class);
+    }
+
+    private List<CloseFilter> getCloseFilters() {
+        return context.getServices(FILTERS, CloseFilter.class);
+    }
+
+    private final class TestEditorContext implements IEditorContext {
+        public IEditorManager getEditorManager() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public void onClose() {
+            // TODO Auto-generated method stub
+            
+        }
+
+        public void onFocus() {
+            // TODO Auto-generated method stub
+            
+        }
+    }
+
+    final static String[] content1 = {
+            "/test", "nt:unstructured",
+                "/test/content", "nt:unstructured",
+                    "jcr:mixinTypes", "mix:referenceable"
+    };
+    final static String[] content2 = {
+                "/test/mirror", "hippo:mirror",
+                    "hippo:docbase", "/test/content",
+            "/config/test-app/cms-editor", "frontend:plugincluster",
+                "frontend:references", "wicket.model",
+                "frontend:services", "wicket.id",
+                "/config/test-app/cms-editor/plugin", "frontend:plugin",
+                    "plugin.class", Editor.class.getName(),
+                    "wicket.id", "${wicket.id}",
+            "/config/test-app/cms-preview", "frontend:plugincluster",
+                "frontend:references", "wicket.model",
+                "frontend:references", "editor.id",
+                "frontend:services", "wicket.id",
+                "/config/test-app/cms-preview/plugin", "frontend:plugin",
+                    "plugin.class", Preview.class.getName(),
+                    "wicket.id", "${wicket.id}",
+                    "wicket.model", "${wicket.model}",
+                "/config/test-app/cms-preview/filter", "frontend:plugin",
+                    "plugin.class", CloseFilter.class.getName(),
+                    "editor.id", "${editor.id}",
+            "/config/test-app/cms-compare", "frontend:plugincluster",
+                "frontend:references", "wicket.model",
+                "frontend:references", "model.compareTo",
+                "frontend:services", "wicket.id",
+                "/config/test-app/cms-compare/plugin", "frontend:plugin",
+                    "plugin.class", Comparer.class.getName(),
+                    "wicket.id", "${wicket.id}",
+                    "wicket.model", "${wicket.model}",
+                    "model.compareTo", "${model.compareTo}",
+    };
+
+    final static String[] cmstestdocument = new String[] {
+            "/${name}", "hippo:handle",
+                "jcr:mixinTypes", "hippo:hardhandle",
+                "/${name}/${name}", "cmstest:document",
+                    "jcr:mixinTypes", "hippo:harddocument"
+            
+    };
+
+    final static String[] plaintestdocument = new String[] {
+            "/${name}", "hippo:handle",
+                "jcr:mixinTypes", "hippo:hardhandle",
+                "/${name}/${name}", "hippo:document",
+                    "jcr:mixinTypes", "hippo:harddocument",
+            
+    };
+
+    static IPluginConfig parameters;
+    static {
+        parameters = new JavaPluginConfig();
+        parameters.put("wicket.id", RENDERERS);
+    }
+    
+    IPluginConfig config;
+    
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        build(session, content1);
+        session.save();
+        build(session, content2);
+        session.save();
+
+        config = new JavaPluginConfig("plugin");
+    }
+
+    protected void createDocument(String name) throws RepositoryException {
+        Map<String, String> pars = new MiniMap(1);
+        pars.put("name", name);
+        build(session, mount("/test/content", instantiate(cmstestdocument, pars)));
+    }
+
+    @Test
+    public void testTemplateType() throws Exception {
+        Node test = root.getNode("test/content").addNode("template", HippoNodeType.NT_TEMPLATETYPE);
+        test.addNode(HippoNodeType.HIPPOSYSEDIT_NODETYPE, HippoNodeType.NT_HANDLE);
+        session.save();
+
+        // open preview
+        DefaultEditorFactoryPlugin factory = new DefaultEditorFactoryPlugin(context, config);
+        IEditor<Node> editor = factory.newEditor(new TestEditorContext(), new JcrNodeModel("/test/content/template"),
+                Mode.VIEW, parameters);
+
+        assertEquals(1, getPreviews().size());
+        assertEquals(0, getEditors().size());
+
+        // edit type
+        editor.setMode(IEditor.Mode.EDIT);
+        assertEquals(0, getPreviews().size());
+        assertEquals(1, getEditors().size());
+
+        // done editing
+        editor.setMode(IEditor.Mode.VIEW);
+        assertEquals(1, getPreviews().size());
+        assertEquals(0, getEditors().size());
+    }
+
+    @Test
+    public void testVersionedDocumentCanOnlyBeOpenedInCompareMode() throws Exception {
+        Map<String, String> pars = new MiniMap<String, String>(1);
+        pars.put("name", "document");
+        build(session, mount("/test/content", instantiate(plaintestdocument, pars)));
+
+        session.save();
+
+        Version version = root.getNode("test/content/document").checkin();
+
+        DefaultEditorFactoryPlugin factory = new DefaultEditorFactoryPlugin(context, config);
+        try {
+            factory.newEditor(new TestEditorContext(), new JcrNodeModel(version), Mode.EDIT, parameters);
+            fail("Could open Version in edit mode");
+        } catch (EditorException e) {
+            // ignore, this is OK
+        }
+        try {
+            factory.newEditor(new TestEditorContext(), new JcrNodeModel(version), Mode.VIEW, parameters);
+            fail("Could open Version in view mode");
+        } catch (EditorException e) {
+            // ignore, this is OK
+        }
+        factory.newEditor(new TestEditorContext(), new JcrNodeModel(version), Mode.COMPARE, parameters);
+    }
+
+    @Test
+    public void testVersionedDocument() throws Exception {
+        Map<String, String> pars = new MiniMap<String, String>(1);
+        pars.put("name", "document");
+        build(session, mount("/test/content", instantiate(plaintestdocument, pars)));
+
+        session.save();
+
+        Version version = root.getNode("test/content/document").checkin();
+
+        DefaultEditorFactoryPlugin factory = new DefaultEditorFactoryPlugin(context, config);
+        IEditor editor = factory.newEditor(new TestEditorContext(), new JcrNodeModel(version), Mode.COMPARE, parameters);
+        List<IRenderService> comparers = getComparers();
+        assertEquals(1, comparers.size());
+        Comparer comparer = (Comparer) comparers.get(0);
+        JcrNodeModel current = (JcrNodeModel) comparer.getModel();
+        JcrNodeModel base = (JcrNodeModel) comparer.getCompareToModel();
+        assertEquals(new JcrNodeModel("/test/content/document/document"), current);
+        assertEquals(new JcrNodeModel(version.getNode("jcr:frozenNode")), base);
+    }
+
+    @Test // Apparently the editor is not closed for non-publishable documents
+    public void deleteDocumentClosesEditor() throws Exception {
+        createDocument("document");
+
+        // open preview
+        DefaultEditorFactoryPlugin factory = new DefaultEditorFactoryPlugin(context, config);
+        JcrNodeModel model = new JcrNodeModel("/test/content/document");
+        IEditor<Node> editor = factory.newEditor(new TestEditorContext(), model, Mode.VIEW, parameters);
+
+        assertEquals(1, getPreviews().size());
+
+        session.getRootNode().getNode("test/content").remove();
+
+        ((IDetachable) editor).detach();
+        ((IRefreshable) editor).refresh();
+
+        assertEquals(0, getPreviews().size());
+    }
+}
---
+++ trunk/translation/frontend/src/main/java/org/hippoecm/frontend/translation/components/document/translate-document.js
@@ -0,0 +1,356 @@
+/*
+ * Copyright 2010 Hippo
+ *
+ * Licensed under the Apache License, Version 2.0 (the  "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+if (Hippo.Translation.Queue == undefined) {
+  (function() {
+
+    Hippo.Translation.Queue = {
+      tasks: [],
+
+      cleanup: function() {
+        for (var i = 0; i < this.tasks.length;) {
+          var handle = this.tasks[i];
+          if (handle.completed) {
+            this.tasks.splice(i, 1);
+          } else {
+            i++;
+          }
+        }
+      },
+
+      enqueue: function(callback, delay) {
+        this.cleanup();
+
+        var task = new Ext.util.DelayedTask();
+        var handle = {
+           extTask: task,
+           started: false,
+           completed: false,
+           callback: callback
+        };
+        this.tasks.push(handle);
+        task.delay(delay, function() {
+          if (!handle.started) {
+            handle.started = true;
+            callback.call(window, function() {
+              handle.completed = true;
+            });
+          }
+        });
+        return handle;
+      },
+
+      cancel: function(handle) {
+        if (!handle.started) {
+          handle.started = true;
+          handle.extTask.cancel();
+          handle.completed = true;
+        }
+      },
+
+      flush: function() {
+        this.cleanup();
+
+        for (var i = 0; i < this.tasks.length; i++) {
+          var handle = this.tasks[i];
+          if (!handle.started) {
+            handle.extTask.cancel();
+            handle.started = true;
+            handle.callback.call(window);
+            handle.completed = true;
+          }
+        }
+      }
+
+    };
+
+  })();
+  Wicket.Ajax.registerPreCallHandler(function() { 
+    Hippo.Translation.Queue.flush(); 
+  });
+}
+
+Hippo.Translation.Document = Ext.extend(Ext.FormPanel, {
+
+  labelWidth: 75, // label settings here cascade unless overridden
+  frame: false,
+  width: 661,
+  defaultType: 'textfield',
+
+  constructor: function(config) {
+    var self = this;
+
+    this.store = config.store;
+    this.codecUrl = config.codecUrl;
+
+    this.resources = config.resources;
+
+    this.emptyImg = config.emptyImg;
+    this.folderImg = config.folderImg;
+    this.documentImg = config.documentImg;
+
+    var imageService = new Hippo.Translation.ImageService(config.imageService);
+    this.imgLeft = imageService.getImage(config.sourceLanguage);
+    this.imgRight = imageService.getImage(config.targetLanguage);
+
+    this.record = null;
+    this.task = null;
+
+    this.updateUrl = function(record, value, callback) {
+        if (window.XMLHttpRequest) {    // Mozilla/Safari
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {     // IE
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xhr.open('POST', self.codecUrl + "&" + Ext.urlEncode({name: value}), callback != undefined);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Wicket-Ajax', "true");
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4) {
+            var obj = Ext.decode(xhr.responseText);
+            record.set('urlfr', obj.data);
+            if (callback != undefined) {
+              callback.call(this);
+            }
+          }
+        };
+        xhr.send(null);
+    },
+
+    this.onKeyUp = function (field, event) {
+      var rec = self.record;
+      if (self.task == null) {
+        self.task = Hippo.Translation.Queue.enqueue(function(callback) {
+          self.task = null;
+          if (Ext.getCmp('urlfr').disabled) {
+            self.updateUrl(rec, field.getRawValue(), callback);
+          } else {
+            callback.call(this);
+          }
+        }, 500);
+      }
+    };
+
+    // the column model has information about grid columns
+    // dataIndex maps the column to the specific data field in
+    // the data store (created below)
+    this.cm = new Ext.grid.ColumnModel({
+      // specify any defaults for each column
+      defaults: {
+        sortable: false, // columns are not sortable by default
+        orderable: false,
+        menuDisabled: true,
+      },
+      columns: [{
+        id: 'name',
+        header: "<img src='" + self.imgLeft + "' style='vertical-align: top;' /> " + self.resources['folder-name'],
+        dataIndex: 'name',
+        width: 323,
+        renderer: self.renderFolder.createDelegate(self, ['url'], 0),
+        // use shorthand alias defined above
+      }, {
+        id: 'namefr',
+        header: "<img src='" + self.imgRight + "' style='vertical-align: top;' /> " + self.resources['folder-name'],
+        dataIndex: 'namefr',
+        width: 334,
+        renderer: self.renderFolder.createDelegate(self, ['urlfr'], 0),
+        editor: new Ext.form.TextField({
+          allowBlank: false,
+          enableKeyEvents: true,
+          listeners: {
+            keyup: function(field, event) {
+              self.onKeyUp(field, event);
+            }
+          }
+        })
+      }],
+      isCellEditable: function(col, row) {
+        var record = self.store.getAt(row);
+        return record.get('editable');
+      }
+    });
+
+    Hippo.Translation.Document.superclass.constructor.call(this, {
+
+      renderTo: config.applyTo,
+      
+      items: [{
+        xtype: 'editorgrid',
+        name: 'grid',
+        store: config.store,
+        width: 666,
+        height: 245,
+        frame: false,
+        clicksToEdit: 1,
+        enableColumnMove: false,
+        enableColumnResize: false,
+        cm: self.cm,
+        sm: new Ext.grid.RowSelectionModel({
+          singleSelect: true,
+          listeners: {
+            rowselect: function(sm, row, rec) {
+              if (rec != self.record) {
+                if (self.task != null) {
+                  Hippo.Translation.Queue.cancel(self.task);
+                  self.task = null;
+                }
+                self.record = rec;
+                self.form.loadRecord(rec);
+                Ext.getCmp('url-edit').setDisabled(!self.record.get('editable'));
+
+                var checked = (self.record.checked || false);
+                Ext.getCmp('urlfr').setDisabled(!checked);
+                Ext.getCmp('url-edit').setValue(checked);
+              }
+            },
+          },
+        }),
+        listeners: {
+      		afteredit: function(e) {
+      			if (e.field == "namefr") {
+      			  if (self.task != null) {
+      			    Hippo.Translation.Queue.cancel(self.task);
+      			    self.task = null;
+      			  }
+              if (Ext.getCmp('urlfr').disabled) {
+                self.updateUrl(e.record, e.value);
+              }
+      			}
+      		}
+      	}
+      }, {
+        xtype: 'compositefield',
+        width: 666,
+        hideLabel: true,
+        frame: true,
+        items: [{
+          xtype: 'fieldset',
+          title: this.resources['url-name'],
+          height: 60,
+          items: [{
+            xtype: 'compositefield',
+            width: 297,
+            hideLabel: true,
+            frame: true,
+            items: [{
+              xtype: 'displayfield',
+              value: '<img src="' + this.imgLeft + '" style="vertical-align: top;" />',
+              width: 25,
+            }, {
+              xtype: 'displayfield',
+              name: 'url',
+              height: 60,
+              width: 225,
+            }]
+          }]
+        }, {
+          xtype: 'fieldset',
+          title: this.resources['url-name'],
+          height: 60,
+          items: [{
+            xtype: 'compositefield',
+            width: 312,
+            hideLabel: true,
+            frame: true,
+            items: [{
+              xtype: 'displayfield',
+              value: '<img src="' + this.imgRight + '" style="vertical-align: top;" />',
+              width: 25,
+            }, {
+              xtype: 'textfield',
+              disabled: true,
+              name: 'urlfr',
+              id: 'urlfr',
+              width: 225,
+              listeners: {
+                blur: function(field) {
+                  self.record.set('urlfr', field.getRawValue());
+                }
+              }
+            }, {
+              xtype: 'checkbox',
+              name: 'edit',
+              id: 'url-edit',
+              disabled: true,
+              value: false,
+              hideLabel: false,
+              boxLabel: self.resources['edit'],
+              listeners: {
+                check: function(chkbox, checked) {
+                  self.record.checked = checked;
+                  Ext.getCmp('urlfr').setDisabled(!checked);
+                }
+              }
+            }]
+          }]
+        }]
+      }]
+    });
+
+    Ext.apply(this, config);
+  },
+
+  initComponent : function() {
+    Hippo.Translation.Document.superclass.initComponent.call(this);
+
+    this.bodyCfg = {
+  		tag: "div",
+  		cls: "x-panel-body"
+	  };
+
+    this.store.on('update', function(store, record, operation) {
+      if (record == this.record) {
+        Ext.getCmp('urlfr').setRawValue(record.get('urlfr'));
+      }
+    }.createDelegate(this));
+    
+    this.store.load();
+  },
+
+  renderFolder: function(col, value, p, record){
+    var indent = {
+      ancestors: 0
+    };
+    this.store.each(function(it) {
+      if (it == record) {
+        return false;
+      }
+      if ((it.get(col) != "") || it.get('editable')) {
+        indent.ancestors++;
+      }
+    });
+
+    var txt = "<img src='" + this.emptyImg + "' width='" + 15 * indent.ancestors + "' height='1'/>";
+    if (value != "" || record.get('editable')) {
+      txt += "<img src='";
+      if (record.data.type == "folder") {
+        txt += this.folderImg;
+      } else {
+        txt += this.documentImg;
+      }
+      txt += "' heigth='10'/> ";
+    }
+
+    if (value == "" && record.get('editable')) {
+      txt += "<font color='#ff0000'><i>" + this.resources['add-translation'] + "</i></font>";
+    } else {
+      txt += String.format("{0}", value);
+    }
+    return txt;
+  },
+
+});
