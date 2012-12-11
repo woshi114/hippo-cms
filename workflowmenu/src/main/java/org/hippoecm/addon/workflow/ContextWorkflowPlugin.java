199e8cd299c04610f9d8f08ee06413b8ffd9127f
Unico Hommes <u.hommes@onehippo.com>
2012-12-11 08:36:55 +0000

    CMS7-6760 stop context on remove of context menu in order to remove listeners

--- workflowmenu/src/main/java/org/hippoecm/addon/workflow/ContextWorkflowPlugin.java
+++ workflowmenu/src/main/java/org/hippoecm/addon/workflow/ContextWorkflowPlugin.java
@@ -25,6 +25,7 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.hippoecm.frontend.model.JcrNodeModel;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
+import org.hippoecm.frontend.service.ServiceContext;
 
 public final class ContextWorkflowPlugin extends AbstractWorkflowPlugin {
     @SuppressWarnings("unused")
@@ -33,7 +34,7 @@ public final class ContextWorkflowPlugin extends AbstractWorkflowPlugin {
     private static final long serialVersionUID = 1L;
 
     public ContextWorkflowPlugin(IPluginContext context, IPluginConfig config) {
-        super(context, config);
+        super(new ServiceContext(context), config);
         Component menu;
         add(menu = new Label("menu"));
         Component v;
@@ -43,6 +44,13 @@ public final class ContextWorkflowPlugin extends AbstractWorkflowPlugin {
         v.setOutputMarkupId(true);
         menu.setOutputMarkupId(true);
         setOutputMarkupId(true);
+    }
+
+    @Override
+    protected void onRemove() {
+        super.onRemove();
+        final ServiceContext context = (ServiceContext) getPluginContext();
+        context.stop();
     }
 
     @Override
