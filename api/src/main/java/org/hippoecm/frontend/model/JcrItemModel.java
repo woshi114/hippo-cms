8139cd8b1ddefc412d602b264d1146eb254d5dd8
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2013-01-09 12:14:38 +0000

    ONS-1023: create hotfix patch

--- api/src/main/java/org/hippoecm/frontend/model/JcrItemModel.java
+++ api/src/main/java/org/hippoecm/frontend/model/JcrItemModel.java
@@ -66,6 +66,9 @@ public class JcrItemModel<T extends Item> extends LoadableDetachableModel<T> {
         super(item);
         relPath = null;
         uuid = null;
+        if (item != null) {
+            save();
+        }
     }
 
     public JcrItemModel(String path) {
