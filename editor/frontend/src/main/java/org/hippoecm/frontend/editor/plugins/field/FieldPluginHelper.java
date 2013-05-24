4142b0668746407fe2be5b2fff2955f40531611b
Frank van Lankvelt <f.vanlankvelt@onehippo.com>
2013-05-24 09:41:15 +0000

    CMS7-6461: remove padding, it is triggered by activating xinha but then messes up the layout

--- editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/field/FieldPluginHelper.java
+++ editor/frontend/src/main/java/org/hippoecm/frontend/editor/plugins/field/FieldPluginHelper.java
@@ -70,7 +70,7 @@ public class FieldPluginHelper implements IDetachable {
                     }
                     field = type.getField(fieldName);
                     if (field == null) {
-                        log.info("Could not find field with name " + fieldName + "; has the field been added without running update-all-content?");
+                        log.warn("Could not find field with name " + fieldName + " in " + type.getName() + "; has the field been added without running update-all-content?");
                     } else if ("*".equals(field.getPath())) {
                         log.warn("Field path * is not supported, field name is " + fieldName);
                         field = null;
