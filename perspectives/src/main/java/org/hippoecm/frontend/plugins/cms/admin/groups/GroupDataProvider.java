60967a5deddd0dd86564b1fd0dbf4fef535c8316
Jannis Bloemendal <j.bloemendal@onehippo.com>
2013-07-26 09:07:15 +0000

    CMS7-7240 User Search searches in too many fields
    Fixed SQL statement.

--- perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/admin/groups/GroupDataProvider.java
+++ perspectives/src/main/java/org/hippoecm/frontend/plugins/cms/admin/groups/GroupDataProvider.java
@@ -38,7 +38,7 @@ public class GroupDataProvider extends SearchableDataProvider<Group> {
 
     private static final String QUERY_GROUP_LIST_TEMPLATE = "SELECT * " +
                     " FROM " + HippoNodeType.NT_GROUP +
-                    " WHERE (hipposys:system <> 'true' OR hipposys:system IS NULL) OR " +
+                    " WHERE (hipposys:system <> 'true' OR hipposys:system IS NULL) AND " +
                           " (  jcr:contains(hipposys:description, '{}') OR " +
                               "jcr:contains(hipposys:members, '{}') OR " +
                               "jcr:contains(hipposys:groups, '{}') " +
