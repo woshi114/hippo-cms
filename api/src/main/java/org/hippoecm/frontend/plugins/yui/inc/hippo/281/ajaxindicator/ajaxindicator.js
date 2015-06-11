1142172b14192db8a3f9047657f6aa25585c2e83
Arthur Bogaart <a.bogaart@onehippo.com>
2015-06-11 13:50:09 +0000

    CMS7-9138: Merged from trunk  - register Wicket events using Wicket.Event.Topic constants  - bind callbacks using Wicket.bind

--- api/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/281/ajaxindicator/ajaxindicator.js
+++ api/src/main/java/org/hippoecm/frontend/plugins/yui/inc/hippo/281/ajaxindicator/ajaxindicator.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2013 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2008-2015 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the  "License");
  * you may not use this file except in compliance with the License.
@@ -13,29 +13,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 /**
  * @description <p>TODO</p>
  * @namespace YAHOO.hippo
  * @requires yahoo, dom
  * @module ajaxindicator
  */
- 
+
  //TODO: might register to a custom layout-processing event to extend the loading indication
- //untill after the layout has processed, instead of just the postAjaxEvent
- 
+ //until after the layout has processed, instead of just the postAjaxEvent
+
 (function() {
   var Dom = YAHOO.util.Dom,
   Lang = YAHOO.util.Lang;
 
   YAHOO.namespace('hippo');
-    
+
   YAHOO.hippo.AjaxIndicator = function(_elId) {
     this.elementId = _elId;
-    
-    var self = this;
-    Wicket.Event.subscribe('/ajax/call/beforeSend', function() { self.show(); });
-    Wicket.Event.subscribe('/ajax/call/complete', function(){ self.hide(); });
+
+    Wicket.Event.subscribe(Wicket.Event.Topic.AJAX_CALL_BEFORE_SEND, Wicket.bind(this.show, this));
+    Wicket.Event.subscribe(Wicket.Event.Topic.AJAX_CALL_COMPLETE, Wicket.bind(this.hide, this));
+
+    if (Ext && Ext.Ajax) {
+      Ext.Ajax.on('beforerequest', this.show, this);
+      Ext.Ajax.on('requestcomplete', this.hide, this);
+      Ext.Ajax.on('requestexception', this.hide, this);
+    }
   };
 
   YAHOO.hippo.AjaxIndicator.prototype = {
@@ -43,7 +48,7 @@
     calls: 0,
     timerID: 0,
     active: true,
-    
+
     getElement: function() {
         return Dom.get(this.elementId);
     },
@@ -58,7 +63,7 @@
         }
         this.active = active;
     },
-    
+
     show: function() {
         if (this.active && this.calls === 0) {
             this.timerID = self.setTimeout("document.body.style.cursor = 'wait';", 750);
@@ -68,24 +73,23 @@
             Dom.setStyle(this.getElement(), 'display', 'block');
         }
     },
-    
+
     hide: function() {
         if (this.calls > 0) {
             this.calls--;
-        } 
+        }
         if (this.active && this.calls === 0) {
             self.clearTimeout(this.timerID);
             document.body.style.cursor = 'default';
             Dom.setStyle(this.getElement(),'display', 'none');
-        }  
+        }
     },
-    
+
     setCursor: function(win, cursor) {
         if (!Lang.isNull(win.document.body)) {
             Dom.setStyle(win.document.body, 'cursor', cursor);
-        }        
+        }
     }
-    
   };
 }());
 
