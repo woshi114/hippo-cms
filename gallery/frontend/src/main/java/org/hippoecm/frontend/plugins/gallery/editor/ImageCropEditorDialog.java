48830511205680e5eaf9dc9c44ccf4578e031506
Saimir Muco <s.muco@onehippo.com>
2016-06-21 17:04:06 +0200

    CMS-10277 [backport to 10.2] fire an event in case binary (image or asset) is uploaded or when image is cropped.
    (cherry picked from commit 32a98903bb31635ce52237df558182db36f07687)

--- gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/editor/ImageCropEditorDialog.java
+++ gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/editor/ImageCropEditorDialog.java
@@ -30,6 +30,7 @@ import javax.imageio.stream.MemoryCacheImageInputStream;
 import javax.jcr.Binary;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.wicket.Component;
@@ -56,6 +57,7 @@ import org.hippoecm.frontend.plugins.gallery.imageutil.ScaleImageOperation;
 import org.hippoecm.frontend.plugins.gallery.imageutil.ScalingParameters;
 import org.hippoecm.frontend.plugins.gallery.model.GalleryException;
 import org.hippoecm.frontend.plugins.gallery.model.GalleryProcessor;
+import org.hippoecm.frontend.plugins.jquery.upload.single.BinaryWorkflowUtils;
 import org.hippoecm.frontend.plugins.standards.image.JcrImage;
 import org.hippoecm.frontend.resource.JcrResourceStream;
 import org.hippoecm.repository.gallery.HippoGalleryNodeType;
@@ -260,6 +262,7 @@ public class ImageCropEditorDialog extends Dialog<Node> {
         int height = jsonObject.getInt("height");
         int left = jsonObject.getInt("left");
         int width = jsonObject.getInt("width");
+        Session session = null;
         try {
             Node originalImageNode = getModelObject().getParent().getNode(HippoGalleryNodeType.IMAGE_SET_ORIGINAL);
             String mimeType = originalImageNode.getProperty(JcrConstants.JCR_MIMETYPE).getString();
@@ -312,10 +315,13 @@ public class ImageCropEditorDialog extends Dialog<Node> {
             cropped.setProperty(JcrConstants.JCR_LASTMODIFIED, Calendar.getInstance());
             cropped.setProperty(HippoGalleryNodeType.IMAGE_WIDTH, dimension.getWidth());
             cropped.setProperty(HippoGalleryNodeType.IMAGE_HEIGHT, dimension.getHeight());
-
+            session = cropped.getSession();
+            session.save();
+            BinaryWorkflowUtils.postBinaryChangedEvent(cropped);
         } catch (GalleryException | IOException | RepositoryException ex) {
             log.error("Unable to create thumbnail image", ex);
             error(ex);
+            BinaryWorkflowUtils.looseSessionChanges(session);
         }
     }
 
