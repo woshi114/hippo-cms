da071978cdba3456b93c817f8f6843f68c317037
Marijan Milicevic <m.milicevic@onehippo.com>
2016-06-29 20:51:27 +0200

    CMS-10023 Clicking twice on internal link toolbar button throws exception and requires to logout to recover - disable button for 300ms to prevent double clicks.

--- richtext/ckeditor/plugins/src/main/resources/ckeditor/plugins/hippopicker/plugin.js
+++ richtext/ckeditor/plugins/src/main/resources/ckeditor/plugins/hippopicker/plugin.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2013-2016 Hippo B.V. (http://www.onehippo.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 (function() {
     "use strict";
 
+    var disableLinkButton = false;
+    var CLICK_DELAY = 300;
     function iterate(object, func) {
         var property;
         for (property in object) {
@@ -107,6 +109,12 @@
         }
 
         function openInternalLinkPickerDialog(selectedLink) {
+            // disable button state for CLICK_DELAY, to prevent double clicking:
+            disableLinkButton = true;
+            editor.getCommand('pickInternalLink').setState(CKEDITOR.TRISTATE_DISABLED);
+            setTimeout(function () {
+                disableLinkButton = false;
+            }, CLICK_DELAY);
             var callbackParameters = getElementParameters(selectedLink, LINK_ATTRIBUTES_PARAMETER_MAP);
 
             Wicket.Ajax.post({
@@ -133,6 +141,9 @@
         }
 
         function updateToolbarButtonState() {
+            if(disableLinkButton) {
+                return;
+            }
             var state = CKEDITOR.TRISTATE_OFF,
                 selection = editor.getSelection();
 
