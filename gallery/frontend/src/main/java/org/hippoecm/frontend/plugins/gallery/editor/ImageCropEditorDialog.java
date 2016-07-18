1edd7cb0cb59f89a21fb0718c1eda52b86dff859
Mathijs den Burger <m.denburger@onehippo.com>
2016-07-18 17:04:35 +0200

    CMS-9985 Store fitView boolean in crop settings
    This way Wicket remembers the value when the CMS is reloaded while
    the crop dialog is shown.

--- gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/editor/ImageCropEditorDialog.java
+++ gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/editor/ImageCropEditorDialog.java
@@ -85,7 +85,7 @@ public class ImageCropEditorDialog extends Dialog<Node> {
     private Dimension configuredDimension;
     private Dimension thumbnailDimension;
     private float compressionQuality;
-    private boolean fitView;
+    private final ImageCropSettings cropSettings;
 
     public ImageCropEditorDialog(IModel<Node> jcrImageNodeModel, GalleryProcessor galleryProcessor) {
         super(jcrImageNodeModel);
@@ -149,13 +149,13 @@ public class ImageCropEditorDialog extends Dialog<Node> {
         thumbnailSize.setOutputMarkupId(true);
         add(thumbnailSize);
         //
-        final ImageCropSettings cropSettings = new ImageCropSettings(regionField.getMarkupId(),
+        cropSettings = new ImageCropSettings(regionField.getMarkupId(),
                 imagePreviewContainer.getMarkupId(),
                 originalImageDimension,
                 configuredDimension,
                 thumbnailDimension,
                 isUpscalingEnabled,
-                fitView,
+                false,
                 thumbnailSize.getMarkupId(true));
 
         if (configuredDimension.width > originalImageDimension.width || configuredDimension.height > originalImageDimension.height) {
@@ -167,7 +167,7 @@ public class ImageCropEditorDialog extends Dialog<Node> {
         }
 
         final ImageCropBehavior imageCropBehavior = new ImageCropBehavior(cropSettings);
-        final IModel<Boolean> fitViewModel = new PropertyModel<>(this, "fitView");
+        final IModel<Boolean> fitViewModel = new PropertyModel<>(this.cropSettings, "fitView");
         final AjaxCheckBox fitViewCheckbox = new AjaxCheckBox("fit-view", fitViewModel) {
             @Override
             protected void onUpdate(AjaxRequestTarget target) {
@@ -203,7 +203,7 @@ public class ImageCropEditorDialog extends Dialog<Node> {
      * Execute the fitInView function on the clientside widget instance
      */
     private void executeFitInView(final AjaxRequestTarget target, final ImageCropBehavior cropBehavior) {
-        final String script = "fitInView(" + fitView + ")";
+        final String script = "fitInView(" + this.cropSettings.isFitView() + ")";
         target.appendJavaScript(cropBehavior.execWidgetFunction(script));
     }
 
