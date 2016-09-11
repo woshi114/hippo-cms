fb10eb174f1ee1ec91494bdc3c4d232e5545af4d
Ate Douma <a.douma@onehippo.com>
2016-09-11 23:22:53 +0200

    [Backport 10.2] CMS-10498: RestProxyServicePlugin can derive and use current request port for rest.uri
    Also only change the default rest.uri in the bootstrap configuration.
    Existing rest.uri configuration will still work fine, unless you want to run multiple
    instances on a single server.
    
    (cherry picked from commit 0ed83f90466fd75d392bf538ba0616fc2202e7af)
    (cherry picked from commit 194a849ae42a4773daedd6eb040db293b472fcff)
    (cherry picked from commit 8ca0595a4a15fb7804e6f8386ed82ccee3ca6f5d)

--- engine/src/main/java/org/hippoecm/frontend/service/restproxy/RestProxyServicePlugin.java
+++ engine/src/main/java/org/hippoecm/frontend/service/restproxy/RestProxyServicePlugin.java
@@ -16,6 +16,8 @@
 package org.hippoecm.frontend.service.restproxy;
 
 import java.lang.annotation.Annotation;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -89,17 +91,13 @@ public class RestProxyServicePlugin extends Plugin implements IRestProxyService 
     public RestProxyServicePlugin(IPluginContext context, IPluginConfig config) {
         super(context, config);
 
-        restUri = config.getString(CONFIG_REST_URI);
-        if (StringUtils.isEmpty(restUri)) {
-            throw new IllegalStateException("No REST service URI configured. Please set the plugin configuration property '"
-                    + CONFIG_REST_URI + "'");
-        }
+        restUri = createRestURI(config.getString(CONFIG_REST_URI), (HttpServletRequest) RequestCycle.get().getRequest().getContainerRequest());
         log.info("Using REST uri '{}'", restUri);
 
         contextPath = config.getString(CONFIG_CONTEXT_PATH);
         if (contextPath != null) {
             if (isValidContextPath(contextPath)) {
-               log.info("Configured contextPath for REST service with uri '{}' is '{}'", restUri, contextPath);
+                log.info("Configured contextPath for REST service with uri '{}' is '{}'", restUri, contextPath);
             } else {
                 throw new IllegalStateException("Invalid context path configured for restUri '"
                         + restUri + "'. Property '"+CONFIG_CONTEXT_PATH+"' must be either missing, empty, or " +
@@ -114,6 +112,27 @@ public class RestProxyServicePlugin extends Plugin implements IRestProxyService 
         log.info("Registering this service under id '{}'", serviceId);
         context.registerService(this, serviceId);
 
+    }
+
+    static String createRestURI(String value, final HttpServletRequest request) {
+        if (StringUtils.isEmpty(value)) {
+            throw new IllegalStateException("No REST service URI configured. Please set the plugin configuration property '"
+                    + CONFIG_REST_URI + "'");
+        }
+        try {
+            URI u = new URI(value);
+            final int portNumber;
+            if (u.getPort() == -1) {
+                portNumber = request.getLocalPort();
+            } else {
+                portNumber = u.getPort();
+            }
+            return new URI(u.getScheme(), u.getUserInfo(), u.getHost(), portNumber, u.getRawPath(), u.getRawQuery(), u.getRawFragment()).toString();
+
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Invalid REST service URI configured. Please correct the plugin configuration property '"
+                    + CONFIG_REST_URI + "'", e);
+        }
     }
 
     @Override
